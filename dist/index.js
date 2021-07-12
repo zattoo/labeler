8198bb1b25c8e1ffcb3ae4af7c1a07c8c54d7670
Nitzan Nashi <nitzan.nashi@zattoo.com>
2021-07-12 18:14:59 +0200

    adjust

--- dist/index.js
+++ dist/index.js
@@ -6127,7 +6127,7 @@ async function run() {
 
     const labelsByGithubAction = await octokit.graphql(`{
       repository(owner: "${repo.owner}", name: "${repo.repo}") {
-        pullRequest(number: "${pull_request.number}") {
+        pullRequest(number: ${pull_request.number}) {
           timelineItems(last: 100, itemTypes: [LABELED_EVENT]) {
           totalCount
           edges {
