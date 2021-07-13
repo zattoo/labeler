e931a29b2ec77973d0ff43edacb97bb64f817066
Nitzan Nashi <nitzan.nashi@zattoo.com>
2021-07-13 11:49:44 +0200

    adjust

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ async function run() {
 
     core.info(changedFiles);
 
-    const labelsFiles = utils.getLabelsFiles(changedFiles, filenameFlag);
+    const labelsFiles = utils.getLabelsFiles([changedFiles[1]], filenameFlag);
     core.info(labelsFiles);
     const labelsFromFiles = await utils.getLabelsFromFiles(labelsFiles);
     core.info(labelsFromFiles);
