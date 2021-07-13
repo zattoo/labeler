8d913f5c51d17de0ac03207c80e0c326e05fc79c
Nitzan Nashi <nitzan.nashi@zattoo.com>
2021-07-13 11:16:02 +0200

    adjust

--- dist/index.js
+++ dist/index.js
@@ -9269,10 +9269,9 @@ async function run() {
 
     core.info(`labelsByGithubAction: ${labelsByGithubAction}`);
 
-    core.info(changedFiles);
-    core.info(changedFiles.split(''));
+    core.info(changedFiles.split(' '));
 
-    const labelsFiles = utils.getLabelsFiles(changedFiles.split(''), filenameFlag);
+    const labelsFiles = utils.getLabelsFiles(changedFiles.split(' '), filenameFlag);
     core.info(labelsFiles);
     const labelsFromFiles = utils.getLabelsFromFiles(labelsFiles);
     core.info(labelsFromFiles);
