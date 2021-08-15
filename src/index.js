46bbe8a51e8599e8ab3d548f12b033589f743ff0
Bogdan Plieshka <bogdanplieshka@gmail.com>
2021-08-15 21:20:55 +0200

    Output

--- src/index.js
+++ src/index.js
@@ -143,21 +143,21 @@ const utils = require('./get-labels');
         const output = Object.keys(matrix).reduce((result, entity) => {
             const prefix = `${entity}:`;
 
-            // const list = labels
-            //     .filter((label) => label.includes(prefix))
-            //     .map((label) => label.split(prefix)[1]);
-            //
-            // if (!list.length) {
-            //     return result;
-            // }
-            //
-            // result = {...result};
+            const list = labels
+                .filter((label) => label.includes(prefix))
+                .map((label) => label.split(prefix)[1]);
 
-            // if (list.includes('common')) {
-            //     result[entity] = matrix[entity];
-            // } else if (list.length) {
-            //     result[entity] = list;
-            // }
+            if (!list.length) {
+                return result;
+            }
+
+            result = {...result};
+
+            if (list.includes('common')) {
+                result[entity] = matrix[entity];
+            } else {
+                result[entity] = list;
+            }
 
             return result;
         }, {});
